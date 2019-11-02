ef1daeed6fa4ec935b009ba6ca365299f07d19fd
doctor <doctor@example.com>
2019-11-02 16:18:44 +0800

    修改baseUrl

--- src/config/index.js
+++ src/config/index.js
@@ -17,8 +17,8 @@ export default {
    * @description api请求基础路径
    */
   baseUrl: {
-    dev: 'http://api.pigzu.com/api/',
-    pro: 'http://api.pigzu.com/api/'
+    dev: 'http://api.vitaloop.cn/api/',
+    pro: 'http://api.vitaloop.cn/api/'
   },
   /**
    * @description 默认打开的首页的路由name值，默认为home
